66a7160b477e65d96bc764d0b22483ac45568e2d
Ms. Firdous <Firdous.mirza@outlook.com>
2026-01-13 16:39:15 +0000

    Weather.jsx

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,31 +1,30 @@
+
 import React, { useEffect, useRef, useState } from "react";
 import './weather.css';
-import Livedatetime from './LiveDateTime';
+import Livedatetime from "./LiveDateTime";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import { faSun } from "@fortawesome/free-solid-svg-icons";
-import { faTint } from "@fortawesome/free-solid-svg-icons/faTint";
-import { faWind } from "@fortawesome/free-solid-svg-icons";
 import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const Weather = () => {
     const[view, setView] = useState(false);
+    const [weatherData, setWeatherData] = useState(false);
+    const [forecast, setForecast] = useState([]);
     const inputRef = useRef();
     function handleClick() {
         setView(true);
     }
-    const allIcons = (iconCode) => {
-        return `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
-    }
-    const [weatherData, setWeatherData] = useState(false);
+    const getIconUrl = (iconCode) => 
+        `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+    
     const search = async (city) => {
         if(city === '') {
             alert('Enter city name');
             return;
         }
         try {
-            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
+            const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${import.meta.env.VITE_WEATHER_APP_ID}`;
             const response = await fetch(url); // fetch/request Weather data for this city from API Server
             const data = await response.json(); // convert JSON text into JS object
             if(!response.ok) {
@@ -33,23 +32,37 @@ const Weather = () => {
                 return;
             }
             console.log(data);
-            setWeatherData({
-                humidity: data.main.humidity,
-                windSpeed: data.wind.speed,
-                temperature: Math.floor(data.main.temp), //Math.floor get the integer val instead of decimal
-                feelsLike: Math.floor(data.main.feels_like),
-                location: data.name,
-                icon: allIcons(data.weather[0].icon),
-                description: data.weather[0].description
-            })
+            const current = data.list[0]; 
+        setWeatherData({
+            humidity: current.main.humidity,
+            windSpeed: current.wind.speed,
+            temperature: Math.floor(current.main.temp),
+            location: data.city.name,
+        });
+        setForecast(data.list.slice(0, 8));
         } catch (error) {
             setWeatherData(false);
             console.error('Error in fetching weather data');           
         }
     }
+    function handleSearch() {
+        const value = inputRef.current.value;
+        if(!value.trim()) {
+            search(value);
+            return;
+        }
+        search(value);
+        inputRef.current.value = "";
+        inputRef.current.focus();
+    }
     useEffect(()=> {
         search('London');
     }, [])
+    useEffect(() => {
+        if (view) {
+           inputRef.current?.focus();
+        }
+    }, [view]);
     return (
         <div className="container">
             <div className="title">
@@ -57,12 +70,9 @@ const Weather = () => {
             </div>
             {view ? <>
             <div className="searchBox">
-                 <input ref={inputRef} type = 'text' placeholder="Enter city"></input>
-                 <FontAwesomeIcon icon={faMagnifyingGlass} className="search-icon" onClick={() => 
-                {  
-                    search(inputRef.current.value);
-                    inputRef.current.value = "";
-                }}/>               
+                 <input ref={inputRef} type = 'text' placeholder="Enter city" onKeyDown={(e) => {
+                    if(e.key === 'Enter') handleSearch() }}></input>
+                 <FontAwesomeIcon icon={faMagnifyingGlass} className="search-icon" onClick={() => handleSearch()}/>               
             </div></> : <>
             <div className="Location_nav">
                 <div className="Location_nav_title">
@@ -78,37 +88,51 @@ const Weather = () => {
             <div className='currentDateTime'>
                <Livedatetime /> 
             </div>
-            <div className="weather-icon">
-               {weatherData.icon ? (
-                <img src={weatherData.icon} className="weather-icon" />
-                ) : (
-                <FontAwesomeIcon icon={faSun} />
-                )}
-                <div className="weather-description">
-                   <span>{weatherData.description}</span>
-                </div>
-            </div>
             <div className="temperature">
                 <p className="degrees">{weatherData.temperature}°C</p>
-                <span className="feels_like_temp"><i>feels_Like {weatherData.feelsLike} °C</i></span>
                 <p className="location">{weatherData.location}</p>
+                <p><span><i>Humidity: {weatherData.humidity} %</i></span></p>
+                <p><span><i>windSpeed: {weatherData.windSpeed} km/h</i></span></p>
             </div>
-            <div className="weather-data">
-                <div className="col">
-                    <FontAwesomeIcon className="humidity-icon" icon = {faTint} />  
-                    <div>  
-                       <p>{weatherData.humidity} %</p>      
-                       <span>Humidity</span>
-                    </div>
-                </div>
-                <div className="col">
-                    <FontAwesomeIcon className="wind-icon" icon = {faWind} />   
-                    <div> 
-                       <p>{weatherData.windSpeed} km/h</p>      
-                       <span>Wind Speed</span>
-                    </div>
-                </div>
-            </div>
+            <div className="forecast">
+                {forecast.map((item, index) => (
+                    <div className="forecast-item" key={index}>
+                        <p className="forecast-date">
+                            {(() => {
+                                const date = new Date(item.dt * 1000); 
+                                const day = date.getDate();
+                                const month = date.toLocaleDateString('en-US', { month: 'short' });
+                                const getOrdinal = (d) => {
+                                    if (d > 3 && d < 21) return 'th';
+                                    switch (d % 10) {
+                                        case 1: return 'st';
+                                        case 2: return 'nd';
+                                        case 3: return 'rd';
+                                        default: return 'th';
+                                    }};
+                                    return `${day}${getOrdinal(day)} ${month}`;
+                                    })()}
+                                    </p>
+                                    <p className="forecast-day">
+                                        {new Date(item.dt * 1000).toLocaleString([], {
+                                            weekday: 'long'
+                                        })}
+                                    </p>
+                                    <p className="forecast-time">
+                                        {new Date(item.dt * 1000).toLocaleString([], {
+                                            hour: 'numeric',
+                                            hour12: true,
+                                        })}
+                                    </p>
+                                    <img
+                                    src = {getIconUrl(item.weather[0].icon)}>
+                                    </img>
+                                    <p className="forecast-temp">
+                                        {Math.floor(item.main.temp)}°C
+                                    </p>
+                                </div>
+                            ))}
+                        </div>
             </> : <></>}
         </div>
     );
